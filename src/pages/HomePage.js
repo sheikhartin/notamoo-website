1006c9fd8217b1cbfb6792d754845c295b2da2a2
Artin Mohammadi <sheikhartin@gmail.com>
2024-08-11 21:23:28 +0330

    Index and 404 pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+const HomePage = () => (
+  <>
+    <Helmet>
+      <title>Home - Notamoo</title>
+    </Helmet>
+
+    <section className="section">
+      <h3 className="subtitle is-3 is-family-secondary has-text-centered">
+        Create Secure Notes for Trusted People!
+      </h3>
+
+      <div className="buttons is-justify-content-center are-medium">
+        <Link className="button" to="/create">
+          Create a Note
+        </Link>
+        <Link className="button" to="/view">
+          Read a Note
+        </Link>
+      </div>
+    </section>
+  </>
+);
+
+export default HomePage;
